3765f0f1064df93b36a723fe953981d2da354a27
tomcrugnale <tom.crugnale+1@gmail.com>
2020-01-10 11:58:41 -0500

    Feature/add dash segment packet side data (#3)
    * dashdec: add segment data as packet metadata
    
    Instead of requiring the consumer to use the FFmpeg internal structures
    in an unsafe manner to get segement information, attach it to each
    packet

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -2326,7 +2326,10 @@ static void recheck_discard_flags(AVFormatContext *s, struct representation **p,
 
 static int dash_read_packet(AVFormatContext *s, AVPacket *pkt)
 {
+    AVDictionary* metadata_dict = NULL;
+    uint8_t* metadata_dict_packed = NULL;
     DASHContext *c = s->priv_data;
+    int metadata_dict_size = 0;
     int ret = 0, i;
     int64_t mints = 0;
     struct representation *cur = NULL;
@@ -2368,12 +2371,31 @@ static int dash_read_packet(AVFormatContext *s, AVPacket *pkt)
     if (!cur) {
         return AVERROR_INVALIDDATA;
     }
+
     while (!ff_check_interrupt(c->interrupt_callback) && !ret) {
         ret = av_read_frame(cur->ctx, pkt);
         if (ret >= 0) {
             /* If we got a packet, return it */
             cur->cur_timestamp = av_rescale(pkt->pts, (int64_t)cur->ctx->streams[0]->time_base.num * 90000, cur->ctx->streams[0]->time_base.den);
             pkt->stream_index = cur->stream_index;
+
+            av_dict_set_int(&metadata_dict, "segNumber", cur->cur_seq_no, 0);
+            if (NULL != cur->cur_seg) {
+                av_dict_set_int(&metadata_dict, "segSize", cur->cur_seg->size, 0);
+            }
+            av_dict_set_int(&metadata_dict, "fragTimescale", cur->fragment_timescale, 0);
+
+            if (cur->n_timelines) {
+                av_dict_set_int(&metadata_dict, "fragDuration", cur->timelines[0]->duration, 0);
+            }
+            else {
+                av_dict_set_int(&metadata_dict, "fragDuration", cur->fragment_duration, 0);
+            }
+
+            metadata_dict_packed = av_packet_pack_dictionary(metadata_dict, &metadata_dict_size);
+            av_dict_free(&metadata_dict);
+            av_packet_add_side_data(pkt, AV_PKT_DATA_STRINGS_METADATA, metadata_dict_packed, metadata_dict_size);
+
             return 0;
         }
         if (cur->is_restart_needed) {
@@ -2385,6 +2407,7 @@ static int dash_read_packet(AVFormatContext *s, AVPacket *pkt)
             cur->is_restart_needed = 0;
         }
     }
+
     return AVERROR_EOF;
 }
 
