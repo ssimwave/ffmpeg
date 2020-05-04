152179a46e6e5d327cb1b26c1b306c4976b19c4f
Chris Olekas <chris.olekas@ssimwave.com>
2020-05-04 10:42:02 -0400

    Use init_buf_sec_offset

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -2314,7 +2314,7 @@ static int hls_read_packet(AVFormatContext *s, AVPacket *pkt)
             int cur_seq_no = pls->cur_seq_no;
             /* If the playlist is VOD then let's cap it to the number of segments */
             if (pls->finished) {
-                if (pkt->pos >= pls->segment_boundary_position) {
+                if (pkt->pos >= pls->segment_boundary_position + pls->init_sec_buf_read_offset) {
                         pls->reported_segment_number++;
                     pls->segment_boundary_position += pls->segments[pls->reported_segment_number]->actual_size;
                 }
