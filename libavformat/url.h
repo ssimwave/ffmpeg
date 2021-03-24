80f3fdc3907ca8866c07ae34ed6ca51d0485be9c
Chris Olekas <chris.olekas@ssimwave.com>
2021-03-24 16:39:54 -0400

    libavform/dashdec - (SSIMWAVE) add support for mpegts parsing and variable passthrough to client applications
    libavform/dashdec - (SSIMWAVE) Use proper method of getting URL context
    
    libavform/dashdec - (SSIMWAVE) Fix incorrect parameters to open_url
    
    First pass getting URLContext backdoor in + DASH changes

--- libavformat/url.h
+++ libavformat/url.h
@@ -46,6 +46,10 @@ typedef struct URLContext {
     const char *protocol_whitelist;
     const char *protocol_blacklist;
     int min_packet_size;        /**< if non zero, the stream is packetized with this min packet size */
+
+    // SSIMWAVE
+    ffurl_read_callback mpegts_parser_injection;
+    void* mpegts_parser_injection_context;
 } URLContext;
 
 typedef struct URLProtocol {
