7ccef716c0111bab52bd37a12be3d0bfc3937e30
Chris Olekas <chris.olekas@ssimwave.com>
2021-04-06 09:08:56 -0400

    libavformat/hls - (SSIMWAVE) Move get actual segment size out of parse_playlist

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -1058,9 +1058,6 @@ static int parse_playlist(HLSContext *c, const char *url,
                     seg_offset = 0;
                 }
 
-                // Get actual segment size
-                seg->actual_size = get_actual_segment_size(seg);
-
                 seg->init_section = cur_init_section;
             }
         }
@@ -1610,6 +1607,9 @@ reload:
 
         v->input_read_done = 0;
         seg = current_segment(v);
+
+        // Get actual segment size
+        seg->actual_size = get_actual_segment_size(seg);
 
         /* load/update Media Initialization Section, if any */
         ret = update_init_section(v, seg);
