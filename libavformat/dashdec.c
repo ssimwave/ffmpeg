5262d1bebedc3f10143fc3b7d3a69fee4698b603
Tom Crugnale <tom.crugnale@ssimwave.com>
2021-05-05 22:24:24 -0400

    dashdec: fix segment selection
    Adjust cur_seq_no by subtracting the first_seq_no if offsetting exists.
    This is required for functions such as calc_next_seg_no_from_timelines()
    which expect the provided seq no to start from a zero index.
    
    Disable some SSIMWAVE specific usage of live_start_index.

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -33,6 +33,14 @@
 #define MAX_BPRINT_READ_SIZE (UINT_MAX - 1)
 #define DEFAULT_MANIFEST_SIZE 8 * 1024
 
+//
+// SSIMWAVE SPECIFIC BUGFIX/FEATURE FLAGS
+//
+#define SSIMWAVE_OFFSET_SEQ_NO 1
+#define SSIMWAVE_MOVE_REP_TIMESTAMP 1
+//#define SSIMWAVE_CALC_CUR_SEG_NO_SELECT_FIRST 1
+//#define SSIMWAVE_GET_FRAGMENT_USE_LIVE_START_INDEX 1
+
 struct fragment {
     int64_t url_offset;
     int64_t size;
@@ -264,6 +272,14 @@ static int64_t get_segment_start_time_based_on_timeline(struct representation *p
     int64_t j = 0;
     int64_t num = 0;
 
+#ifdef SSIMWAVE_OFFSET_SEQ_NO
+    if (pls->n_timelines) {
+        if (cur_seq_no > pls->first_seq_no) {
+            cur_seq_no -= pls->first_seq_no;
+        }
+    }
+#endif  // SSIMWAVE_OFFSET_SEQ_NO
+
     if (pls->n_timelines) {
         for (i = 0; i < pls->n_timelines; i++) {
             if (pls->timelines[i]->starttime > 0) {
@@ -316,10 +332,17 @@ static int64_t calc_next_seg_no_from_timelines(struct representation *pls, int64
         num++;
     }
 
+#ifdef SSIMWAVE_OFFSET_SEQ_NO
+    return pls->first_seq_no;
+
+finish:
+    return num + pls->first_seq_no;
+#else
     return -1;
 
 finish:
     return num;
+#endif // SSIMWAVE_OFFSET_SEQ_NO
 }
 
 static void free_fragment(struct fragment **seg)
@@ -1396,12 +1419,18 @@ static int64_t calc_cur_seg_no(AVFormatContext *s, struct representation *pls)
             num = pls->first_seq_no;
         } else if (pls->n_timelines) {
             av_log(s, AV_LOG_TRACE, "in n_timelines mode\n");
+#ifdef SSIMWAVE_CALC_CUR_SEG_NO_SELECT_FIRST
+            num = pls->first_seq_no;
+#else
             start_time_offset = get_segment_start_time_based_on_timeline(pls, 0xFFFFFFFF) - 60 * pls->fragment_timescale; // 60 seconds before end
             num = calc_next_seg_no_from_timelines(pls, start_time_offset);
+#ifndef SSIMWAVE_OFFSET_SEQ_NO
             if (num == -1)
                 num = pls->first_seq_no;
             else
                 num += pls->first_seq_no;
+#endif  // SSIMWAVE_OFFSET_SEQ_NO
+#endif  // SSIMWAVE_CALC_CUR_SEG_NO_SELECT_FIRST
         } else if (pls->fragment_duration){
             av_log(s, AV_LOG_TRACE, "in fragment_duration mode fragment_timescale = %"PRId64", presentation_timeoffset = %"PRId64"\n", pls->fragment_timescale, pls->presentation_timeoffset);
             if (pls->presentation_timeoffset) {
@@ -1473,6 +1502,9 @@ static void move_timelines(struct representation *rep_src, struct representation
         rep_src->timelines = NULL;
         rep_src->n_timelines = 0;
         rep_dest->cur_seq_no = rep_src->cur_seq_no;
+#ifdef SSIMWAVE_MOVE_REP_TIMESTAMP
+        rep_dest->cur_timestamp = rep_src->cur_timestamp;
+#endif
     }
 }
 
@@ -1490,6 +1522,9 @@ static void move_segments(struct representation *rep_src, struct representation 
         rep_dest->last_seq_no = calc_max_seg_no(rep_dest, c);
         rep_src->fragments = NULL;
         rep_src->n_fragments = 0;
+#ifdef SSIMWAVE_MOVE_REP_TIMESTAMP
+        rep_dest->cur_timestamp = rep_src->cur_timestamp;
+#endif
     }
 }
 
