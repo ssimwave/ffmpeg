d3fefdbda9f2d89a25e5cad51b10c898f3453276
Nicholas Vanderzwet <nicholas.vanderzwet@ssimwave.com>
2021-10-26 15:43:10 -0400

    More compile errors

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -242,11 +242,12 @@ typedef struct HLSContext {
 } HLSContext;
 
 static int64_t get_actual_segment_size(struct playlist *pls, struct segment* seg) {
+    HLSContext *c = pls->parent->priv_data;
     AVIOContext* pb = NULL;
     AVDictionary *opts = NULL;
     int64_t actual_size = -1;
 
-    av_dict_copy(&opts, pls->ctx->avio_opts, 0);
+    av_dict_copy(&opts, c->avio_opts, 0);
     if (pls->ctx->io_open(pls->ctx, &pb, seg->url, AVIO_FLAG_READ, &opts)) {
         actual_size = avio_seek(pb, 0, AVSEEK_SIZE);
     }
