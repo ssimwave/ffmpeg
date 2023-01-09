fce46e8e7c4ba1362b84a76fcf69394726896260
Tom Crugnale <tom.crugnale@ssimwave.com>
2023-01-09 11:32:25 -0500

    Revert

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -2480,16 +2480,15 @@ static MXFTrack* mxf_get_dovi_metadata_track(MXFContext* mxf)
 
 static int mxf_init_dovi_metadata_stream(MXFContext* mxf, AVStream* st)
 {
-    //int ret = 0;
+    int ret = 0;
 
     if (!mxf->dovi_global_metadata) {
         av_log(NULL, AV_LOG_TRACE, "no Dolby Vision global metadata found in metadata sets\n");
         return AVERROR_INVALIDDATA;
     }
 
-    //ret = av_dict_set(&st->metadata, "dovi_global_metadata", mxf->dovi_global_metadata->data, 0 /* flags */);
-    //return ret;
-    return 0;
+    ret = av_dict_set(&st->metadata, "dovi_global_metadata", mxf->dovi_global_metadata->data, 0 /* flags */);
+    return ret;
 }
 
 static int mxf_add_dovi_metadata_stream(MXFContext* mxf)
