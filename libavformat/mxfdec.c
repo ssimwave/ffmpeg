2cdc65355327f3d182f06aa1080df9d8bcdfe427
Tom Crugnale <tom.crugnale@ssimwave.com>
2023-01-30 16:52:21 -0500

    mxfdec: Disable DoVi metadata extract by default

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -4441,7 +4441,7 @@ static const AVOption options[] = {
       offsetof(MXFContext, eia608_extract), AV_OPT_TYPE_BOOL, {.i64 = 0}, 0, 1,
       AV_OPT_FLAG_DECODING_PARAM },
     { "dovi_metadata_extract", "extract Dolby Vision (ie. Prototype HDR) metadata",
-      offsetof(MXFContext, phdr_metadata_extract), AV_OPT_TYPE_BOOL, {.i64 = 1}, 0, 1,
+      offsetof(MXFContext, phdr_metadata_extract), AV_OPT_TYPE_BOOL, {.i64 = 0}, 0, 1,
       AV_OPT_FLAG_DECODING_PARAM },
     { NULL },
 };
