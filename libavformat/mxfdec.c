85b757fda53f98e01690379c3cbb849382387d9f
Tom Crugnale <tom.crugnale@ssimwave.com>
2023-02-17 10:48:51 -0500

    mxfdec: Fix a couple of compile issues

--- libavformat/mxfdec.c
+++ libavformat/mxfdec.c
@@ -4341,7 +4341,7 @@ static int mxf_read_close(AVFormatContext *s)
 
     if (mxf->generic_stream_data) {
         for (i = 0; i < mxf->generic_stream_data_count; ++i) {
-            av_freep(&generic_stream_data->data);
+            av_freep(&mxf->generic_stream_data[i].data);
         }
         av_freep(&mxf->generic_stream_data);
     }
