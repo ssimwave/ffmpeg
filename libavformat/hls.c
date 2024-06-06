5e1882d14a3d7d32563e34db321bb5683636327b
jesse auerbach <jesse.auerbach@ssimwave.com>
2024-06-06 12:46:37 -0700

    Halve default playlist reload interval

--- libavformat/hls.c
+++ libavformat/hls.c
@@ -1528,8 +1528,8 @@ static int update_init_section(struct playlist *pls, struct segment *seg)
 static int64_t default_reload_interval(struct playlist *pls)
 {
     return pls->n_segments > 0 ?
-                          pls->segments[pls->n_segments - 1]->duration :
-                          pls->target_duration;
+                          pls->segments[pls->n_segments - 1]->duration / 2 :
+                          pls->target_duration / 2;
 }
 
 static int playlist_needed(struct playlist *pls)
