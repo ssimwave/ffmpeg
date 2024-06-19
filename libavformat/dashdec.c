c539fad1965b61827e4c8e7ebff6f1a870abbd7e
nvanderzwet-ssimwave <70330032+nvanderzwet-ssimwave@users.noreply.github.com>
2024-06-19 15:55:16 -0400

    Fix repeating segments in live MPD playback (#40)
    * Infer the start number from the previous segment information if its is missing (instead of resetting to 0 every reload)
    
    * Sleep/retry if an attempt to read past of the end of the playlist occurs (instead of replaying the edge segment)
    
    * Put changes behind use_timeline_segment_offset_correction config, remove redundant/debug method
    
    * Improve manifest reload delay logic (add config, default to more reasonable value). Better handle (rare/unlikely) case where we are segment numbers get messed up if we far too far behind while processing
    
    * Simplify delay_reload logic, fix some C90 warnings
    
    * Attempt to guess the start_number based on timeline (when its missing)
    
    * Skip the fix_start_number on representations that haven't been opened yet

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -123,6 +123,7 @@ struct representation {
 
     ffurl_read_callback mpegts_parser_input_backup;
     void* mpegts_parser_input_context_backup;
+    int found_start_number;
 };
 
 typedef struct DASHContext {
@@ -158,6 +159,8 @@ typedef struct DASHContext {
     int fetch_completed_segments_only;
     int use_independent_segment_fetch_for_obtaining_size;
     int start_on_live_edge;
+    int max_reload;
+    int reload_retry_interval;
     // END SSIMWAVE ADDITIONS
 
     int is_live;
@@ -998,6 +1001,7 @@ static int parse_manifest_representation(AVFormatContext *s, const char *url,
         val = get_val_from_nodes_tab(fragment_templates_tab, 4, "startNumber");
         if (val) {
             rep->start_number = rep->first_seq_no = (int64_t) strtoll(val, NULL, 10);
+            rep->found_start_number = 1;
             av_log(s, AV_LOG_TRACE, "rep->first_seq_no = [%"PRId64"]\n", rep->first_seq_no);
             xmlFree(val);
         }
@@ -1067,6 +1071,7 @@ static int parse_manifest_representation(AVFormatContext *s, const char *url,
         val = get_val_from_nodes_tab(segmentlists_tab, 3, "startNumber");
         if (val) {
             rep->start_number = rep->first_seq_no = (int64_t) strtoll(val, NULL, 10);
+            rep->found_start_number = 1;
             av_log(s, AV_LOG_TRACE, "rep->first_seq_no = [%"PRId64"]\n", rep->first_seq_no);
             xmlFree(val);
         }
@@ -1330,6 +1335,7 @@ static int parse_manifest(AVFormatContext *s, const char *url, AVIOContext *in)
                 av_log(s, AV_LOG_TRACE, "c->publish_time = [%"PRId64"]\n", c->publish_time);
             } else if (!av_strcasecmp(attr->name, "minimumUpdatePeriod")) {
                 c->minimum_update_period = get_duration_insec(s, val);
+                c->reload_retry_interval = (c->minimum_update_period * 1000) / 2;
                 av_log(s, AV_LOG_TRACE, "c->minimum_update_period = [%"PRId64"]\n", c->minimum_update_period);
             } else if (!av_strcasecmp(attr->name, "timeShiftBufferDepth")) {
                 c->time_shift_buffer_depth = get_duration_insec(s, val);
@@ -1554,6 +1560,7 @@ static void move_timelines(struct representation *rep_src, struct representation
         free_timelines_list(rep_dest);
         rep_dest->timelines    = rep_src->timelines;
         rep_dest->n_timelines  = rep_src->n_timelines;
+        rep_dest->start_number = rep_src->start_number;
         rep_dest->first_seq_no = rep_src->first_seq_no;
         rep_dest->last_seq_no = calc_max_seg_no(rep_dest, c);
         rep_src->timelines = NULL;
@@ -1581,6 +1588,76 @@ static void move_segments(struct representation *rep_src, struct representation 
     }
 }
 
+static int64_t guess_start_number(DASHContext* c, struct representation* rep) {
+    int64_t duration = rep->fragment_duration;
+    int64_t startNumber = rep->start_number;
+    if (c->use_timeline_segment_offset_correction && !rep->found_start_number && rep->timelines && rep->n_timelines) {
+        // Best guess: ((wall clock time - availabilityStartTime ) / (duration / timescale ))
+        if (!duration) {
+            // The timeline segment duration can vary +/-50% between segments, but its our best guess.
+            duration = rep->timelines[0]->duration;
+        }
+        if (duration) {
+            startNumber = ((get_current_time_in_sec() - c->availability_start_time) * rep->fragment_timescale)/ (duration);
+            av_log(c, AV_LOG_DEBUG, "Guessing start_number from timeline: [%"PRId64"]\n", startNumber);
+        }
+    }
+    return startNumber;
+}
+
+static void fix_start_number(DASHContext* c, struct representation** old_reps, struct representation** new_reps, int n_reps, const char* label) {
+    int64_t last_end_time = 0;
+    int64_t last_seq_no = 0;
+    int64_t new_last_seq_no = 0;
+    int64_t new_start_number = 0;
+
+    if (!c->use_timeline_segment_offset_correction) {
+        return;
+    }
+
+    for (int i = 0; i < n_reps; i++) {
+        struct representation *last_rep = old_reps[i];
+        struct representation *new_rep = new_reps[i];
+        if (!new_rep->found_start_number && new_rep->timelines && new_rep->n_timelines > 0) {
+            if (!last_rep->last_seq_no) {
+                // We haven't called open_demux_for_component on this representation yet, skip for now. 
+                continue;
+            }
+
+            // The representation is using timeline mode - and has no start number hint. So try to guess the start 
+            // number by finding where the last segment in the previous version of the representation is in the current
+            // representation version. We can use this to find the number of segments which have been dropped since the
+            // representation was updated. We can then infer the new start_number based on this information, and the
+            // previous version of the representations start_number.
+            last_end_time = get_segment_start_time_based_on_timeline(c, last_rep, last_rep->last_seq_no) / last_rep->fragment_timescale;
+            last_seq_no = calc_next_seg_no_from_timelines(c, new_rep, last_end_time * new_rep->fragment_timescale - 1);
+            if (last_seq_no < 0) {
+                // Previous edge sequence is outside of the current playlist... which means that an entire timeShiftBufferDepth worth
+                // of time (or more) has passed and every segment in the playlist is new...
+                // Try to estimate the start sequence and segment number from the timeline
+                av_log(c, AV_LOG_WARNING, "Previous edge segment not found in manifest, discontinuity suspected\n");
+                new_start_number = guess_start_number(c, new_rep);
+                if (new_start_number < last_rep->last_seq_no + 1) {
+                    av_log(c, AV_LOG_DEBUG, "Fixing %s timeline. start_number: [%"PRId64"], new: [%"PRId64"]\n",
+                           label, new_rep->start_number, last_rep->last_seq_no + 1);
+                    new_start_number = last_rep->last_seq_no + 1;
+                }
+                new_rep->start_number = new_rep->first_seq_no = new_start_number;
+                continue;
+            }
+            new_last_seq_no = calc_max_seg_no(new_rep, c);
+            av_log(c, AV_LOG_DEBUG, "Checking if %s start_number needes fixing, last_end_time: [%"PRId64"] last_seq_no: [%"PRId64"], new_last_seq_no: [%"PRId64"], last_start_number: [%"PRId64"], new_start_number: [%"PRId64"]\n",
+                   label, last_end_time, last_seq_no, new_last_seq_no, last_rep->start_number, new_rep->start_number);
+            new_start_number = last_rep->start_number + (new_last_seq_no - last_seq_no);
+            if (new_rep->start_number != new_start_number) {
+                // Update start/first_seq_no (last_seq_no will be recalculated in move_timelines/move_segments)
+                av_log(c, AV_LOG_DEBUG, "Fixing %s timeline. start_number: [%"PRId64"], new: [%"PRId64"]\n",
+                       label, new_rep->start_number, new_start_number);
+                new_rep->start_number = new_rep->first_seq_no = new_start_number;
+            }
+        }
+    }
+}
 
 static int refresh_manifest(AVFormatContext *s)
 {
@@ -1625,6 +1702,14 @@ static int refresh_manifest(AVFormatContext *s)
                n_subtitles, c->n_subtitles);
         return AVERROR_INVALIDDATA;
     }
+
+    /* Some encoders will NOT include a startNumber in the segment timeline, startNumber IS optional in the spec, but the rest of the code
+     * uses the start number (and first_seq_no and last_seq_no) to perform segment selection when in timeline mode. So in the case where the playlist
+     * is in timeline mode, and start_number is NOT set, we need to fix-up the start_number/first_seq_no/last_seq_no based on the segment times instead. */
+    fix_start_number(c, videos, c->videos, n_videos, "video");
+    fix_start_number(c, audios, c->audios, n_audios, "audio");
+    fix_start_number(c, subtitles, c->subtitles, n_subtitles, "subtitles");
+
     /* It is possible for the demuxer to be processing at the live edge and waiting for a segment in the future.
      * When that happens 'cur_seq_no' can be past the end of the timelines indicated by the MPD.
      * The functions below that attempt to calculate the next segment number based on the timeline data will end up
@@ -1693,6 +1778,7 @@ static int get_current_fragment(struct representation *pls, struct fragment** ne
     int err = 0;
     int64_t min_seq_no = 0;
     int64_t max_seq_no = 0;
+    int64_t old_max_seq_no = 0;
     struct fragment *seg = NULL;
     struct fragment *seg_ptr = NULL;
     DASHContext *c = pls->parent->priv_data;
@@ -1725,18 +1811,27 @@ static int get_current_fragment(struct representation *pls, struct fragment** ne
     if (c->is_live) {
         min_seq_no = calc_min_seg_no(pls->parent, pls);
         max_seq_no = calc_max_seg_no(pls, c);
+        old_max_seq_no = max_seq_no;
 
         if (pls->timelines || pls->fragments) {
             err = refresh_manifest(pls->parent);
             if (AVERROR_INPUT_CHANGED == err) {
                 return err;
             }
+            min_seq_no = calc_min_seg_no(pls->parent, pls);
+            max_seq_no = calc_max_seg_no(pls, c);
         }
         if (pls->cur_seq_no < min_seq_no) {
-            av_log(pls->parent, AV_LOG_VERBOSE, "old fragment: cur[%"PRId64"] min[%"PRId64"] max[%"PRId64"]\n", (int64_t)pls->cur_seq_no, min_seq_no, max_seq_no);
+            av_log(pls->parent, AV_LOG_VERBOSE, "old fragment: cur[%"PRId64"] min[%"PRId64"] max[%"PRId64"]\n",
+                   (int64_t)pls->cur_seq_no, min_seq_no, max_seq_no);
             pls->cur_seq_no = calc_cur_seg_no(pls->parent, pls);
-        } else if (pls->cur_seq_no > max_seq_no) {
-            av_log(pls->parent, AV_LOG_VERBOSE, "new fragment: min[%"PRId64"] max[%"PRId64"]\n", min_seq_no, max_seq_no);
+        } else if (pls->cur_seq_no > old_max_seq_no) {
+            av_log(pls->parent, AV_LOG_VERBOSE, "new fragment: cur[%"PRId64"] min[%"PRId64"] max[%"PRId64"] (prev_max[%"PRId64"])\n",
+                   (int64_t)pls->cur_seq_no, min_seq_no, max_seq_no, old_max_seq_no);
+            if (pls->cur_seq_no > max_seq_no) {
+                av_log(pls->parent, AV_LOG_VERBOSE, "new fragment outside playlist availability");
+                return AVERROR(EAGAIN);
+            }
         }
         seg = av_mallocz(sizeof(struct fragment));
         if (!seg) {
@@ -1753,12 +1848,12 @@ static int get_current_fragment(struct representation *pls, struct fragment** ne
         if (!pls->url_template) {
             av_log(pls->parent, AV_LOG_ERROR, "Cannot get fragment, missing template URL\n");
             av_free(seg);
-            return NULL;
+            return 0;
         }
         tmpfilename = av_mallocz(c->max_url_size);
         if (!tmpfilename) {
             av_free(seg);
-            return NULL;
+            return 0;
         }
         ff_dash_fill_tmpl_params(tmpfilename, c->max_url_size, pls->url_template, 0, pls->cur_seq_no, 0, get_segment_start_time_based_on_timeline(c, pls, pls->cur_seq_no));
         seg->url = av_strireplace(pls->url_template, pls->url_template, tmpfilename);
@@ -1769,7 +1864,7 @@ static int get_current_fragment(struct representation *pls, struct fragment** ne
                 av_log(pls->parent, AV_LOG_ERROR, "Cannot resolve template url '%s'\n", pls->url_template);
                 av_free(tmpfilename);
                 av_free(seg);
-                return NULL;
+                return 0;
             }
         }
         av_free(tmpfilename);
@@ -1917,9 +2012,24 @@ static int64_t seek_data(void *opaque, int64_t offset, int whence)
     return AVERROR(ENOSYS);
 }
 
+static void delay_reload(DASHContext *c, int64_t reload_count)
+{
+    int delay = 0;
+
+    // Attempt first reload immediately, otherwise wait the reload
+    if (reload_count > 1) {
+        delay = c->reload_retry_interval * 1000;
+    }
+    if (delay > 0) {
+        av_log(c, AV_LOG_DEBUG, "Segment not ready (reload_count: %"PRId64"), retrying again in %dms\n", reload_count, delay/1000);
+        av_usleep(delay);
+    }
+}
+
 static int read_data(void *opaque, uint8_t *buf, int buf_size)
 {
     int ret = 0;
+    int reload_count = 0;
     struct representation *v = opaque;
     DASHContext *c = v->parent->priv_data;
 
@@ -1937,8 +2047,16 @@ static int read_data(void *opaque, uint8_t *buf, int buf_size)
 restart:
     if (!v->input) {
         free_fragment(&v->cur_seg);
+        reload_count++;
+        if (reload_count > c->max_reload)
+            return AVERROR_EOF;
         ret = get_current_fragment(v, &v->cur_seg);
-        if (0 != ret) {
+        if (ret == AVERROR(EAGAIN) && c->is_live) {
+            // Fragment not ready yet, sleep and try again
+            delay_reload(c, reload_count);
+            goto restart;
+        }
+        else if (0 != ret) {
             goto end;
         }
         if (!v->cur_seg) {
@@ -1980,8 +2098,16 @@ restart:
 
     /* check the v->cur_seg, if it is null, get current and double check if the new v->cur_seg*/
     if (!v->cur_seg) {
+        reload_count++;
+        if (reload_count > c->max_reload)
+            return AVERROR_EOF;
         ret = get_current_fragment(v, &v->cur_seg);
-        if (0 != ret) {
+        if (ret == AVERROR(EAGAIN) && c->is_live) {
+            // Fragment not ready yet, sleep and try again
+            delay_reload(c, reload_count);
+            goto restart;
+        }
+        else if (0 != ret) {
             goto end;
         }
         if (!v->cur_seg) {
@@ -2007,7 +2133,6 @@ end:
         urlc->mpegts_parser_injection = v->mpegts_parser_input_backup;
         urlc->mpegts_parser_injection_context = v->mpegts_parser_input_context_backup;
     }
-
     return ret;
 }
 
@@ -2104,11 +2229,14 @@ fail:
 
 static int open_demux_for_component(AVFormatContext *s, struct representation *pls)
 {
+    DASHContext *c = s->priv_data;
     int ret = 0;
     int i;
 
     pls->parent = s;
 
+    // Guess the start number using the timeline and duration (if not set)
+    pls->start_number = pls->first_seq_no = guess_start_number(c, pls);
     if (!pls->last_seq_no) {
         pls->last_seq_no = calc_max_seg_no(pls, s->priv_data);
     }
@@ -2567,6 +2695,10 @@ static const AVOption dash_options[] = {
         OFFSET(use_independent_segment_fetch_for_obtaining_size), AV_OPT_TYPE_BOOL, {.i64 = 0}, 0, 1, FLAGS},
     { "start_on_live_edge", "Start processing at the latest segment for live manifests",
         OFFSET(start_on_live_edge), AV_OPT_TYPE_BOOL, {.i64 = 0}, 0, 1, FLAGS},
+    {"max_reload", "Maximum number of times a list is attempted to be reloaded during live (dynamic) playback",
+        OFFSET(max_reload), AV_OPT_TYPE_INT, {.i64 = 100}, 0, INT_MAX, FLAGS},
+    {"reload_retry_interval", "Interval in ms to wait before retrying playlist reload. If not set, defaults to mimumumUpdatePeriod/2",
+        OFFSET(reload_retry_interval), AV_OPT_TYPE_INT, {.i64 = -1}, 0, INT_MAX, FLAGS},
     {NULL}
 };
 
