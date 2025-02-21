6d0b29a0830567fc988ac844c6e2de3ab656d637
nvanderzwet-ssimwave <70330032+nvanderzwet-ssimwave@users.noreply.github.com>
2025-02-21 13:09:45 -0500

    DASH (live): Fail quicker if the playlists are missing segments (#56)
    Fail open right away if there are no segments in the playlist, instead of waiting on timeouts and reloads that never work.

--- libavformat/dashdec.c
+++ libavformat/dashdec.c
@@ -2288,6 +2288,11 @@ static int open_demux_for_component(AVFormatContext *s, struct representation *p
     // Guess the start number using the timeline and duration (if not set)
     pls->start_number = pls->first_seq_no = guess_start_number(c, pls);
     if (!pls->last_seq_no) {
+        if (c->is_live && !calc_max_seg_no(pls, s->priv_data)) {
+            av_log(pls->parent, AV_LOG_WARNING, "DASH manifest has no valid segments\n");
+            ret = AVERROR_INVALIDDATA;
+            goto fail;
+        }
         pls->last_seq_no = calc_max_seg_no(pls, s->priv_data);
     }
     pls->cur_seq_no  = calc_cur_seg_no(s, pls);
